e817c594afe09d4303ec8f45debcc1f8526585ab
Daniel Nascimento Pimentel <dnprocks@yahoo.com.br>
2020-08-20 16:21:28 -0300

    setup express with decorators + route test

---
+++ src/controllers/template.ts
@@ -0,0 +1,11 @@
+import {Controller, Get} from '@overnightjs/core';
+import {Request, Response} from 'express';
+
+@Controller('template')
+export class TemplateController {
+
+    @Get('')
+    public getTemplateForLoggedUser(_: Request, res: Response): void {
+        res.send({"status": "ok"});
+    }
+}
---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import './util/module-alias';
+import { Server } from '@overnightjs/core';
+import bodyParser from 'body-parser';
+import { TemplateController } from './controllers/template';
+import { Application } from 'express';
+
+export class SetupServer extends Server {
+
+
+    constructor(private port = 3000) {
+        super();
+    }
+
+    public init(): void {
+        this.setupExpress();
+        this.setupController();
+    }
+
+    private setupExpress(): void {
+        this.app.use(bodyParser.json());
+    }
+
+    private setupController(): void {
+        const templateController = new TemplateController();
+        this.addControllers([templateController])
+    }
+
+    public getApp(): Application {
+        return this.app;
+    }
+}
